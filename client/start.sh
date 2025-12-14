d663c114b7d6f78121e419b4bdc7a5d746bc41fe
Biscuitmunch <biscuitmunch0@gmail.com>
2025-12-15 00:35:22 +1300

    feat: use shell

--- client/start.sh
+++ client/start.sh
@@ -1,3 +1,1 @@
-@ECHO OFF
-SET PORT=5000
 pnpm install --reporter=silent && pnpm build && pnpm start
--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,2 @@
-@ECHO OFF
+export PORT=5000
 pnpm install --reporter=silent && pnpm build && pnpm start
